6a23687002d3d9c91d79c462056e098ac84b23c3
trinstone <2021.09.16@online.devetagimnazija.edu.rs>
2024-05-17 15:57:43 +0200

    Metoda za GornjiLevi cosak dela za crtanje
    Odredjivanje koordinata gornjeg levog coska od kog se crta pozadina

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AgeomProj
 {
-    partial class Form1
+    partial class frmUvod
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmUvod
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1030, 630);
+            this.Name = "frmUvod";
+            this.Text = "Ageom";
+            this.Load += new System.EventHandler(this.frmUvod_Load);
+            this.ResizeEnd += new System.EventHandler(this.frmUvod_ResizeEnd);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmUvod_Paint);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -4,17 +4,50 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AgeomProj
 {
-    public partial class Form1 : Form
+    public partial class frmUvod : Form
     {
-        public Form1()
+        double Levi, Gornji;  
+        public frmUvod()
         {
             InitializeComponent();
+        }
+        public (double, double) LeviGornji()
+        {
+            double x = -1, y = -1;
+            if (this.Width <= this.Height) 
+            {
+                x = (this.Height - this.Width) / 2;
+                y = 0;
+                return (x, y);
+            }
+            else
+            {
+                x = 0;
+                y = (this.Height - this.Width) / 2; 
+                return (x, y);
+            }
+        }
+        private void frmUvod_Load(object sender, EventArgs e)
+        {
+            //80 kvadratica vodoravno, 45 horizontalno
+            (Levi, Gornji) = LeviGornji();
+        }
+
+        private void frmUvod_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void frmUvod_ResizeEnd(object sender, EventArgs e)
+        {
+            (Levi, Gornji) = LeviGornji();
         }
     }
 }
