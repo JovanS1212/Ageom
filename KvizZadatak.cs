b7e88e889de2bb60593aec3e1a693cd66dcd4fb9
trinstone <116944957+trinstone@users.noreply.github.com>
2024-05-19 14:46:15 +0200

    Klase kviz i slobodan zadatak
    Polovicno uradjene klase kviz i slobodan zadatak

---
+++ KvizZadatak.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeomProj
+{
+    internal class KvizZadatak : Zadatak
+    {
+        public override double[] Odgovor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public KvizZadatak(string pitanje, TimeSpan vreme, double[] odgovor) : base(pitanje, vreme, odgovor)
+        {
+        }
+    }
+}
---
+++ SlobodanZadatak.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeomProj
+{
+    public enum FormaResenja {broj, pravaEks, pravaImp, krug, elipsaHiperbola, parabola }
+    internal class SlobodanZadatak : Zadatak
+    {
+        public override double[] Odgovor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Hint { get; set; }
+        public FormaResenja FormaResenja {  get; set; }
+        public SlobodanZadatak(string pitanje, TimeSpan vreme, double[] odgovor, FormaResenja formaResenja) : base(pitanje, vreme, odgovor)
+        {
+            FormaResenja = formaResenja;
+        }
+    }
+}
