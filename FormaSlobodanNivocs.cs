ad868fc67c6b45851c186bce1994e126ab0cdc4b
IrinaCv <104566560+IrinaCv@users.noreply.github.com>
2024-05-26 18:05:00 +0200

    Ucitavanje txt fajla

--- FormaSlobodanNivocs.cs
+++ FormaSlobodanNivocs.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,23 @@ namespace AgeomProj
         private void frmSlobodanNivo_Load(object sender, EventArgs e)
         {
        
+        }
+       
+        private Zadatak[] UnosSaFajla(string imeFajla)
+        {
+            Zadatak[] zadaci = new Zadatak[4];
+            StreamReader red = new StreamReader(imeFajla);
+            int brojac = 0;
+            while (!red.EndOfStream)
+            {
+                string[] infoZad = red.ReadLine().Split('!');
+                SlobodanZadatak a = new SlobodanZadatak(infoZad[0], TimeSpan.Parse(infoZad[1]), infoZad[2], (FormaResenja)(Convert.ToInt32(infoZad[3])), infoZad[4]);
+                zadaci[brojac] = a;
+                brojac++;
+            }
+            red.Close();
+
+           return zadaci;
         }
     }
 }
--- SlobodanZadatak.cs
+++ SlobodanZadatak.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AgeomProj
 {
-    public enum FormaResenja {broj, pravaEks, pravaImp, krug, elipsaHiperbola, parabola }
+    public enum FormaResenja {broj,tacka, pravaEks, pravaImp, krug, elipsaHiperbola, parabola,tekst }
     internal class SlobodanZadatak : Zadatak, IVrsteZadataka
     {
         
@@ -14,7 +14,7 @@ namespace AgeomProj
         
         public string Hint { get; set; }
 
-        public SlobodanZadatak(string pitanje, TimeSpan vreme,string hint, string odgovor, FormaResenja formaResenja) : base(pitanje, vreme, odgovor)
+        public SlobodanZadatak(string pitanje, TimeSpan vreme,string hint,FormaResenja formaResenja, string odgovor ) : base(pitanje, vreme, odgovor)
         {
             FormaResenja = formaResenja;
             Hint = hint;
